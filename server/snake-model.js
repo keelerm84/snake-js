418f3d926ccb0e6550e1fc86b4213ca0a36d0e74
Matthew M. Keeler <keelerm84@gmail.com>
2013-08-15 20:02:57 -0500

    Handles initial growing until configured length is attained

--- server/snake-model.js
+++ server/snake-model.js
@@ -12,13 +12,20 @@ Snake.prototype.step = function() {
   var x_step = 0;
   var y_step = 0;
 
-  if (this.direction === 'n') { y_step = -1 }
-  else if (this.direction === 'e') { x_step = 1 }
-  else if (this.direction === 's') { y_step = 1 }
-  else if (this.direction === 'w') { x_step = -1 }
+  if (this.direction === 'n') { y_step = -1; }
+  else if (this.direction === 'e') { x_step = 1; }
+  else if (this.direction === 's') { y_step = 1; }
+  else if (this.direction === 'w') { x_step = -1; }
 
-  current = nodes[0];
-  this.nodes.push({ x: current.x + x_step, y: current.y + y_step });
+  var current = this.nodes[0];
+  this.nodes.unshift({ x: current.x + x_step, y: current.y + y_step });
+
+  // If the current length is less than the number of nodes we
+  // currently have, then there is no need to pop.  This typically
+  // means we're growing during the first moves of the game.
+  if ( this.len >= this.nodes.length ) return;
+
+  this.nodes.pop();
 };
 
 module.exports = Snake;
--- test/server/snake-test.js
+++ test/server/snake-test.js
@@ -7,7 +7,7 @@ describe('SnakeModel', function() {
   var snake;
 
   beforeEach(function() {
-    snake = new Snake(0, 0);
+    snake = new Snake(0, 0, 3);
   });
 
   it('should have an initial direction of north', function() {
@@ -23,11 +23,21 @@ describe('SnakeModel', function() {
     snake.nodes[0].y.should.equal(0);
   });
 
+  it('should grow until we have reached the configured length', function() {
+    snake.len.should.equal(3);
+    snake.nodes.length.should.equal(1);
+
+    var expectedLengths = [2, 3, 3];
+    expectedLengths.forEach(function (expectedLength) {
+      snake.step();
+      snake.nodes.length.should.equal(expectedLength);
+    });
+  });
+
   describe('invalid parameters', function() {
     it('should default length to 1 if invalid', function() {
       snake = new Snake(0, 0, -3);
       snake.len.should.equal(1);
     });
   });
-
 });
